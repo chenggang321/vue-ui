a4d9904d71a7b613c1fa5ec2f5f357626f47a9b6
chengganghy <chenggang@ruyucapital.com>
2019-02-18 14:34:24 +0800

    添加nav和aside

--- examples/router/index.js
+++ examples/router/index.js
@@ -1,19 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import navConf from '../nav.config.json'
 
 Vue.use(Router)
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/button',
-      name: 'Button',
-      component: r => require.ensure([], () => r(require('../docs/button.md')))
+
+let routes = []
+
+Object.keys(navConf).forEach((header) => {
+  routes = routes.concat(navConf[header])
+})
+
+let addComponent = (router) => {
+  router.forEach((route) => {
+    if (route.items) {
+      addComponent(route.items)
+      routes = routes.concat(route.items)
+    } else {
+      route.component = r => require.ensure([], () =>
+        r(require(`../docs/${route.name}.md`)))
     }
-  ]
+  })
+}
+addComponent(routes)
+
+export default new Router({
+  routes: routes
 })
